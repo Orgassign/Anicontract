92a4cb5652203524d260e1fb7afc90f6e07f0297
Hivering <91452236+Hivering@users.noreply.github.com>
2022-07-27 16:43:09 +0800

    Update IERC20.sol

--- contract/interfaces/IERC20.sol
+++ contract/interfaces/IERC20.sol
@@ -5,18 +5,13 @@ interface IERC20 {
 
     function totalSupply() external view returns (uint256);
 
-
     function balanceOf(address account) external view returns (uint256);
-
 
     function transfer(address recipient, uint256 amount) external returns (bool);
 
-
     function allowance(address owner, address spender) external view returns (uint256);
 
-
     function approve(address spender, uint256 amount) external returns (bool);
-
 
     function transferFrom(
         address sender,
@@ -24,9 +19,7 @@ interface IERC20 {
         uint256 amount
     ) external returns (bool);
 
-
     event Transfer(address indexed from, address indexed to, uint256 value);
-
 
     event Approval(address indexed owner, address indexed spender, uint256 value);
 }
